a7530f5b0e47517eb40231ccb139d4c05b5f4c93
Razvan <tangent.jotey@gmail.com>
2020-10-23 00:50:00 +0200

    update

--- src/main/java/io/github/tncrazvan/quarkus/tools/action/VoidAction.java
+++
@@ -1,9 +0,0 @@
-package io.github.tncrazvan.quarkus.tools.action;
-
-/**
- *  Make a simple void callback.
- * @author Razvan Tanase
- */
-public interface VoidAction{
-    public abstract void callback();
-}
